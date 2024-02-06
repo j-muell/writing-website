013bd372fe481dd1675a3967d8fe7f7bdaff6e72
Jason Mueller <jasonmueller17@yahoo.ca>
2024-02-06 11:41:57 -0500

    init writing website

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from "react";
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="flex justify-center">
+        <p className="text-cyan-200 bg-black">This is me testing tailwindcss</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
